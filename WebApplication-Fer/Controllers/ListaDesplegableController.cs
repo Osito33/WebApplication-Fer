cd90ef38447941c2a110eb4583a0f8d5b387af1a
Horacio <morales557221@gmail.com>
2024-05-15 16:55:05 -0600

    Commit

---
+++ WebApplication-Fer/Controllers/ListaDesplegableController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication_Fer.Data;
+
+namespace WebApplication_Fer.Controllers
+{
+    public class ListaDesplegableController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ListaDesplegableController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var productos = _context.Productos.Select(u => u.Descripcion).ToList();
+            ViewBag.Usuarios = productos;
+            return View();
+        }
+
+    }
+
+}
